608abc68016472049cb57e815bd8c7bd5af3b02e
Lina Marcela Avila Moreno <limavilam@hotmail.com>
2022-12-20 17:03:35 -0500

    adding a modification of the code in file selection

--- ExercisesCap7/ExeVII1.py
+++ ExercisesCap7/ExeVII1.py
@@ -1,5 +1,7 @@
-rtxt = open ('mbox-short.txt')
-for line in rtxt:
-    #Se coloca este para eliminar los espacios en blanco
-    line_w = line.rstrip()
+rtxt = open ('c:/Users/limav/OneDrive/Documentos/Machine/P4EB/Exer-Python4Everybody/PythonExeBook/ExercisesCap7/mbox-short.txt')
+print (rtxt)
+
+for line in rtxt: 
+    print (line)
+    line_w = line.rstrip ()
     print (line_w.upper())
