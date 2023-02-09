bde440a9fac4c85f226ec74fa0fe3c21a44fe079
Lina Marcela Avila Moreno <limavilam@hotmail.com>
2023-02-08 22:49:05 -0500

    Update ExerciseXII5, changes in the code

--- ExercisesCap12/ExeXII5.py
+++ ExercisesCap12/ExeXII5.py
@@ -1,16 +1,19 @@
 import socket
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
+my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
-mysock.send(cmd)
-#Si imprimo cmd me va salir bytes
+my_sock.send(cmd)
 
-data = mysock.recv(512)
+data = my_sock.recv(512)
 message = data.decode()
 header_end_pos = message.find('\r\n\r\n') + 4   # Finds the end of header
+                                            # Adds four to exclude:'\r\n\r\n'
 print(message[header_end_pos:])
-
-
-
+while True:                                 # Header in the first data only
+    data = my_sock.recv(512)
+    if not data:
+        break
+    print(data.decode())
+my_sock.close()
 
